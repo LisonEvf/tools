2cf6024e263d094ff85ebbd691155b510c3b45dd
LisonEvf <LisonEvf@gmail.com>
2018-05-30 16:22:06 +0800

    manifest生成文件

---
+++ makeManifest.php
@@ -0,0 +1,96 @@
+<?php
+$DIR_PUB = dirname(__FILE__);
+$DIR_ROOT = $DIR_PUB.'/..';
+
+$dirPublish=$argv[1];
+$updateServer=$argv[2];
+
+sprint("请输入版本号：");
+$input = trim(fgets(STDIN));
+
+if($input == "")
+	$version = "1.0.0.0";
+else {
+	$version = $input;
+}
+println($version);
+println($updateServer);
+
+
+$resfilelist = tree($dirPublish."/res");
+$srcfilelist = tree($dirPublish."/src");
+$filelist = array_merge_recursive($resfilelist,$srcfilelist);
+
+$versionfile = fopen("version.txt", "w");
+fwrite($versionfile, $version);
+$vfile = fopen($DIR_ROOT."/version.manifest", "w");
+$pfile = fopen($DIR_ROOT."/project.manifest", "w");
+
+$headStr = '{
+"packageUrl" : "'.$updateServer.'",
+"remoteVersionUrl" : "'.$updateServer.'version.manifest",
+"remoteManifestUrl" : "'.$updateServer.'project.manifest",
+"version" : "'.$version.'",
+"engineVersion" : "Cocos2d-JS v3.10.1"';
+
+fwrite($vfile, $headStr."\n}");
+fwrite($pfile, $headStr.',
+"assets" : {');
+$index = 0;
+foreach ($filelist as $file){
+	$index ++;
+	$str = '
+	"'.substr($file, strlen($dirPublish)+1).'" : {
+		"md5" : "'.md5_file($file).'",
+		"size" : '.filesize($file).'
+	}';
+	if($index<sizeof($filelist))
+		$str .= ',';
+	fwrite($pfile, $str);
+}
+fwrite($pfile, "\n\t}\n}");
+
+copy($DIR_ROOT."/version.manifest",$dirPublish."/version.manifest");
+copy($DIR_ROOT."/project.manifest",$dirPublish."/project.manifest");
+
+function tree($directory)
+{
+	$filelist = array();
+    $mydir = dir($directory);
+    while($file = $mydir->read())
+    	if((is_dir("$directory/$file")) AND ($file!=".") AND ($file!=".."))
+            $filelist = array_merge_recursive($filelist,tree("$directory/$file"));
+        else if (($file!=".") AND ($file!="..") AND ($file!=".DS_Store") AND (substr($file,0,2)!="._") AND ($file!="logo.png") AND ($file!="bg_018.jpg") AND ($file!="favicon.png"))
+        	array_push($filelist,"$directory/$file");
+    $mydir->close();
+    return $filelist;
+}
+
+function sprint($text) {
+	if(PHP_OS=="WINNT")
+		echo $text;
+	else
+		echo(chr(27) . "[35m" . "$text" . chr(27) . "[0m");
+}
+
+function println($text) {
+	if(PHP_OS=="WINNT")
+		echo $text."\n";
+	else
+		echo(chr(27) . "[35m" . "$text" . chr(27) . "[0m\n");
+}
+
+function error($text) {
+	if(PHP_OS=="WINNT")
+		echo $text."\n";
+	else
+		echo(chr(27) . "[31m" . "$text" . chr(27) . "[0m\n");
+}
+
+function success($text) {
+	if(PHP_OS=="WINNT")
+		echo $text."\n";
+	else
+		echo(chr(27) . "[32m" . "$text" . chr(27) . "[0m\n");
+}
+?>
